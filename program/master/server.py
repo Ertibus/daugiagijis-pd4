6070f2ec172c3583d837517fbf98ab81a4dfc0e0
Ertibus <47594900+Ertibus@users.noreply.github.com>
2021-05-30 23:14:05 +0300

    Basic Multiclient Server

--- program/master/server.py
+++ program/master/server.py
@@ -2,16 +2,29 @@
 
 import socket
 import sys
-import io
-try:
-    from PIL import Image
-except ImportError:
-    import Image
+
+import time
+import threading
 
 class Server:
     host = "localhost"
     port = 6060
     server_socket = socket.socket()
+
+    NUMBER_OF_THREADS = 5
+    active_connections = []
+    active_addresses = []
+
+    def start(self):
+        self.bind_socket()
+        
+        connection_thread = threading.Thread(target=self.accept_connections)
+        connection_thread.deamon = True
+        connection_thread.start()
+
+        master_thread = threading.Thread(target=self.send_msg_all)
+        master_thread.deamon = True
+        master_thread.start()
 
     def bind_socket(self):
         try:
@@ -22,32 +35,41 @@ class Server:
             print("Socket Binding error: " + str(err) + ",\t Retrying...")
             self.bind_socket()
 
-    def socket_accept(self):
-        print("Waiting for client to connect")
+    def accept_connections(self):
+        self.close_connections()
 
-        connection, address = self.server_socket.accept()
-        print("Connection established with:\t{}:{}".format(address[0], address[1]))
+        while True:
+            connection, address = self.server_socket.accept()
 
-        connection.send("hello client".encode('utf-8'))
+            self.active_connections.append(connection)
+            self.active_addresses.append(address)
+
+            print("\nConnection established with: [ {}:{} ]".format(address[0], address[1]))
+
+            connection.send("hello client".encode('utf-8'))
+
+            if len(self.active_connections) == 0:
+                break
+    def close_connections(self):
+        for conn in self.active_connections:
+            conn.close()
+        del self.active_connections[:]
+        del self.active_addresses[:]
+
+    def send_msg_all(self):
+        while True:
+            msg = input("Message to send:")
+            if msg == 'quit':
+                self.close_connections()
+                break;
+            for connection in self.active_connections:
+                connection.send(msg.encode('utf-8'))
         
-        data = connection.recv(4096)
-        if data.decode('utf-8') == "give file":
-            with open("./images/1Text.jpg", 'rb') as file:
-                data = file.read()
-                connection.send(data)
-                pass
-            print("File established!")
-
-        self.send_image()
-        
-        connection.close()
-
     def send_image(self):
         with open("./images/1Text.jpg", 'rb'):
             pass
 
 if __name__ == '__main__':
     server = Server()
-    server.bind_socket()
-    server.socket_accept()
+    server.start()
 
--- program/slave/client.py
+++ program/slave/client.py
@@ -1,14 +1,14 @@
 import socket
-import os
-
 from PIL import Image
 import pytesseract
 
 
 class Client:
+
     host = "localhost"
     server_port = 6060
     client_socket = socket.socket()
+
 
     def read_image(self, image:Image):
         text = pytesseract.image_to_string(image, lang='eng')
@@ -18,15 +18,21 @@ class Client:
     def connect(self):
         self.client_socket.connect((self.host, self.server_port))
         data = self.client_socket.recv(4096)
+
         if data.decode('utf-8') == "hello client":
             print("Connection established!")
-        self.client_socket.send("give file".encode('utf-8'))
 
         while data:
             data = self.client_socket.recv(4096)
-            with open('test.jpg', 'ab') as file:
-                file.write(data)
 
+            if data.decode('utf-8') == "hello client":
+                print("Connection established!")
+
+            if data.decode('utf-8') == "hi":
+                print("hello server!")
+
+            if data.decode('utf-8') == "foo":
+                print("bar!")
 
 if __name__ == '__main__':
     client = Client()
