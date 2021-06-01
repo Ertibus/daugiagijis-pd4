208ce46a860663eaae5f11dce238d593ea002b77
Ertibus <47594900+Ertibus@users.noreply.github.com>
2021-06-01 16:19:12 +0300

    Working TCP Connection

--- client/client.py
+++ client/client.py
@@ -1,17 +1,23 @@
 # ./program/master/server.py
 
 import socket
-import os
 import io
+import os
+import time
 
 from PIL import Image
 import pytesseract
+
+import tqdm
 
 
 class Client:
     HOST = "localhost"
     SERVER_PORT = 6060
     BUFFER_SIZE = 8192
+    MAX_WORKERS = 5
+
+    SEPARATOR = "<SEPARATOR>"
 
     client_socket = socket.socket()
 
@@ -24,49 +30,87 @@ class Client:
 
     def connect(self):
         self.client_socket.connect((self.HOST, self.SERVER_PORT))
-        while True:
-            print("Waiting for message from server")
-            data = self.client_socket.recv(self.BUFFER_SIZE)
-            msg = data.decode('utf-8')
-            
-            if msg.split(' ')[0] == "hello_client":
-                print("Connection established! I'm: {}".format(msg.split(' ')[1]))
-            elif msg == "send work":
-                self.recieve_images()
-                results = self.process_images()
-                self.return_results(results)
-                
+        print("Waiting for message from server")
+        data = self.client_socket.recv(self.BUFFER_SIZE)
+        msg = data.decode('utf-8')
+        if msg.split(' ')[0] == "hello_client":
+            print("Connection established! I'm: {}".format(msg.split(' ')[1]))
+            self.client_socket.send("hello_server".encode('utf-8'))
+            while True:
+                msg = self.client_socket.recv(self.BUFFER_SIZE).decode('utf-8')
+                if msg == 'WORK':
+                    self.recieve_images()
+                    results = self.process_images()
+                    self.return_results(results)
+                elif msg == "NJOB" or not msg:
+                    break
+                else:
+                    raise ValueError("Response Mismatch")
 
     def recieve_images(self):
         while True:
-            print("Ready to recieve work")
-            self.client_socket.send("ready".encode('utf-8'))
-            data = self.client_socket.recv(self.BUFFER_SIZE)
-            msg = data.decode('utf-8')
-            if msg == "catch":
-                img_data=b''
-                while True:
-                    buffer = self.client_socket.recv(self.BUFFER_SIZE)
-                    img_data += buffer
-                    if len(buffer) < self.BUFFER_SIZE:
-                        break;
-                rcv_img = Image.open(io.BytesIO(img_data))
-                self.images_list.append(rcv_img)
-            if msg == "end":
-                print("Recieved work files!")
+            self.client_socket.send('INFO'.encode('utf-8'))
+            file_info = self.client_socket.recv(self.BUFFER_SIZE).decode('utf-8')
+
+            if file_info == 'DONE':
                 break;
 
+            filename, filesize = file_info.split(self.SEPARATOR)
+
+            filename = os.path.basename(filename)
+            filesize = int(filesize)
+
+            self.client_socket.send('DATA'.encode('utf-8'))
+            image_stream = io.BytesIO()
+            with tqdm.tqdm(range(filesize), f"Receiving {filename}:", unit="B", unit_scale=True, unit_divisor=1024) as progress:
+                while True:
+                    buffer_read = self.client_socket.recv(self.BUFFER_SIZE)
+                    image_stream.write(buffer_read)
+                    progress.update(len(buffer_read))
+                    if len(buffer_read) < self.BUFFER_SIZE:
+                        break;
+            img_data = image_stream
+            rcv_img = Image.open(img_data)
+            self.images_list.append(rcv_img)
+
+
     def process_images(self):
-        result = ""
-        for image in self.images_list:
-            print("processing image..")
-            result += pytesseract.image_to_string(image, lang='eng')
-        return result
+        read_text = ""
+        pbar = tqdm.tqdm(self.images_list, "Processing images:")
+        for image in pbar:
+            read_text += self.tesseract(image)
+            self.client_socket.send("TEXT".encode('utf-8'))
+            msg = self.client_socket.recv(self.BUFFER_SIZE).decode('utf-8')
+            if msg != 'CHEK':
+                raise ValueError("Response Mismatch")
+
+        pbar.close()
+        return read_text
+
+
+    def tesseract(self, image:Image):
+        while True:
+            with open('test_lock', 'rb') as lock:
+                lock_read = lock.read()
+                if lock_read == b'0':
+                    break;
+                time.sleep(0.01)
+        with open('test_lock', 'wb') as lock:
+            lock.write(b'1')
+        ret_val = pytesseract.image_to_string(image, lang='eng')
+        with open('test_lock', 'wb') as lock:
+            lock.write(b'0')
+        return ret_val
+
 
     def return_results(self, result):
-        print("writing results..")
-        with open("resultfile.txt", "w") as file:
-            file.write(result)
+        print("Sending back the results..\n")
+        del self.images_list[:]
+        self.client_socket.send("SCND".encode('utf-8'))
+        msg = self.client_socket.recv(self.BUFFER_SIZE).decode('utf-8')
+        if msg != 'SEND':
+            raise ValueError("Response Mismatch")
+        self.client_socket.sendall(result.encode('utf-8'))
 
 if __name__ == '__main__':
     client = Client()
--- server/server.py
+++ server/server.py
@@ -2,6 +2,8 @@
 
 import socket
 import io
+import os
+import math
 
 import time
 import threading
@@ -11,27 +13,49 @@ from PIL import Image
 class Server:
 
     RETRY_COUNT = 5
+    RETRY_WAIT = 2 #seconds
     WORK_SIZE = 5
     BUFFER_SIZE = 4096
+    SEPARATOR = "<SEPARATOR>"
 
     host = "localhost"
     port = 6060
-    server_socket = socket.socket()
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    active_connections = []
-    active_addresses = []
+    client_list = []
 
+    processed= 0 # 0 - Listening, 1 - Processing
+
+#           START METHODS
     def start(self):
+        self._work_lock = threading.Lock()
+        self._state_lock = threading.Lock()
+        self._write_lock = threading.Lock()
+        self._socket_lock = threading.Lock()
         self.bind_socket()
+
+        directory='./images'
+        self.image_paths = self.get_image_paths(directory)
+        self.create_work()
+
+
+        with open('out/results.txt', 'w') as fd:
+            pass
         
         connection_thread = threading.Thread(target=self.accept_connections)
-        connection_thread.deamon = True
         connection_thread.start()
 
-        master_thread = threading.Thread(target=self.send_msg_all)
-        master_thread.deamon = True
-        master_thread.start()
+        #time.sleep(5)
 
+
+    def get_image_paths(self, dir_path):
+        file_list = []
+        for file in os.listdir(dir_path):
+            file_list.append(dir_path + "/" + file)
+        return file_list
+
+#           SOCKET METHODS
+    # Bind socket to host and port
     def bind_socket(self, attempt=0):
         try:
             self.server_socket.bind((self.host, self.port))
@@ -40,66 +64,161 @@ class Server:
         except socket.error as err:
             if self.RETRY_COUNT > attempt:
                 print("Socket Binding error: " + str(err) + ", Retrying...")
+                time.sleep(self.RETRY_WAIT)
                 self.bind_socket(attempt+1)
             else:
                 raise Exception("Socket Binding error: " + str(err) + ", Max Retry tries reached. :(")
 
-
+    # Listen for connections and add them to connection list
     def accept_connections(self):
         self.close_connections()
-
+        self._state_lock.acquire()
         while True:
             connection, address = self.server_socket.accept()
 
-            client_id = len(self.active_connections)
+            client_id = len(self.client_list)
 
-            self.active_connections.append(connection)
-            self.active_addresses.append(address)
+            new_connection = dict()
+            new_connection['connection'] = connection
+            new_connection['address'] = address
+
+            self.client_list.append(new_connection)
+            th_client = threading.Thread(target=self.client_logic, args=(new_connection, ))
 
             print("Connection established with: [ {}:{} ]\n".format(address[0], address[1]))
             connection.send("hello_client {}".format(client_id).encode('utf-8'))
-
-    def close_connections(self):
-        for conn in self.active_connections:
-            conn.close()
-        del self.active_connections[:]
-        del self.active_addresses[:]
-
-    def send_msg_all(self):
-        while True:
-            msg = input("Message to send:")
-            if msg == 'quit':
-                self.close_connections()
-                break
-            elif msg == 'send_image':
-                self.send_images()
-                continue
-            for connection in self.active_connections:
-                connection.send(msg.encode('utf-8'))
-        
-    def send_images(self):
-        for i, connection in enumerate(self.active_connections):
-            connection.send("send work".encode('utf-8'))
-
-            for ii in range(5):                
-                data = connection.recv(self.BUFFER_SIZE)
-                if data.decode('utf-8') == 'ready':
-                    connection.send("catch".encode('utf-8'))
-
-                    img = Image.open(f'./images/{ii + 1}Text.jpg')
-                    buffer = io.BytesIO()
-                    img.save(buffer, format='JPEG')
-                    connection.send(buffer.getvalue())
-
             data = connection.recv(self.BUFFER_SIZE)
-            if data.decode('utf-8') == 'ready':
-                connection.send("end".encode('utf-8'))
+            if data.decode('utf-8') == 'hello_server':
+                print("Client said hello")
+                if self._state_lock.locked():
+                    self._state_lock.release()
+                th_client.start()
+                pass
+            print("Switching server state")
+
+    # Close all active connections and clear client_list
+    def close_connections(self):
+        for conn in self.client_list:
+            conn['connection'].close()
+        del self.client_list[:]
+
+        
+#           CLIENT LOGIC
+    def client_logic(self, connection_dict):
+        print('init client')
+        while self.work_load_list:
+            try:
+                self._state_lock.acquire()
+                self._state_lock.release()
+                print('looking for a job')
+                job = self.find_work()
+                while job:
+                    print('found a job')
+                    self.send_images(connection_dict, job)
+                    job = self.find_work()
+            except Exception as err:
+                print(str(err))
+                break;
+        connection_dict['connection'].close()
+
+                
+
+    def create_work(self):
+        self.work_load_list = []
+        for i in range(math.ceil(len(self.image_paths) / self.WORK_SIZE)):
+            self.work_load_list.append(self.image_paths[self.WORK_SIZE*i:self.WORK_SIZE*i+self.WORK_SIZE])
+
+    def find_work(self):
+        self._work_lock.acquire()
+        if len(self.work_load_list) > 0:
+            work = self.work_load_list.pop()
+        else:
+            work = None
+        self._work_lock.release()
+        print(len(self.work_load_list))
+        return work
+
+    def append_work(self, work):
+        self._work_lock.acquire()
+        print('Appending work')
+        self.work_load_list.append(work)
+        self._work_lock.release()
+
+    def send_images(self, worker, work_load):
+        ret_stream = io.BytesIO()
+        try:
+            connection = worker['connection']
+            address = worker['address']
+
+            print("Started sending data over: {}".format(address))
+            connection.send('WORK'.encode('utf-8'))
+            self._socket_lock.acquire()
+            for wrk_image in work_load:                
+                print(wrk_image + " SENDING")
+                msg = connection.recv(self.BUFFER_SIZE).decode('utf-8')
+                if msg != 'INFO':
+                    raise ValueError('Connection mismatch')
+
+                img = Image.open(wrk_image)
+                buffer = io.BytesIO()
+                img.save(buffer, format='JPEG')
+
+                filesize = buffer.getbuffer().nbytes
+                connection.send(f'{wrk_image}{self.SEPARATOR}{filesize}'.encode('utf-8'))
+
+                msg = connection.recv(self.BUFFER_SIZE).decode('utf-8')
+                if msg != 'DATA':
+                    raise ValueError('Connection mismatch')
+
+                connection.send(buffer.getvalue())
+
+            self._socket_lock.release()
+            msg = connection.recv(self.BUFFER_SIZE).decode('utf-8')
+            if msg != 'INFO':
+                raise ValueError('Connection mismatch')
+
+            connection.send('DONE'.encode('utf-8'))
+            print("Done sending data over: {}".format(address))
+
+            while True:
+                msg = connection.recv(self.BUFFER_SIZE).decode('utf-8')
+                if msg == 'TEXT':
+                    connection.send('CHEK'.encode('utf-8'))
+                    print("x")
+                elif msg == 'SCND':
+                    connection.send('SEND'.encode('utf-8'))
+                    while True:
+                        buffer = connection.recv(self.BUFFER_SIZE)
+                        ret_stream.write(buffer)
+                        if len(buffer) < self.BUFFER_SIZE:
+                            break
+                    break
+                else:
+                    raise ValueError('Unexpected respone from client')
+
+        except Exception as err:
+            if self._socket_lock.locked():
+                self._socket_lock.release()
+            self.append_work(work_load)
+            raise Exception(str(err))
+        
+
+
+        self.processed += 5
+        header = "\n\n===========================================\n\t{}{}\tImages:{}\n=======================================\n\n".format(self.processed - 5, self.processed, work_load)
+        self.write_result(header + ret_stream.getvalue().decode('utf-8'))
+
+
+    def write_result(self, result):
+        self._write_lock.acquire()
+        print("Writing results")
+        with open('out/results.txt', 'a') as fd:
+            fd.write(result)
+        self._write_lock.release()
+
             
 
 if __name__ == '__main__':
     server = Server()
-    try:
-        server.start()
-    except Exception as err:
-        print(err)
+    server.start()
 
